6fbfe46a5937f6b3e65d5239f2d1c01e92826c73
syl3n7 <claudiosilva@protonmail.com>
2022-02-24 13:22:41 +0000

    update

---
+++ CGS-intro/Assets/Scripts/pog.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Starting the beast..");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("We are online (:");
+        
+    }
+}
